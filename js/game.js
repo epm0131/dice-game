3d4c05ac71ccd464540ce582ac86f7be811905c7
Ettore Merendino <merendino.ettore@gmail.com>
2016-10-26 16:36:15 -0400

    I am making sure the lay out of the code relates to the assignment objectives. Created object in the game and made the correct properties.  Also added an array to the dice roll.

--- js/game.js
+++ js/game.js
@@ -1,74 +1,60 @@
-var dice = {};//dice object
-var diceArray = []; //dice Array
-
-
-var counter = 0;
-
-function setDateTime()
-{
-  var now = new Date();
-  dice.startDate = ( '' + now.getFullYear() + "-" + (now.getMonth()+ 1) + "-" + now.getDate());
-  dice.startTime = ( now.getHours() +":"+ now.getMinutes() );
-  return "Game Started " + dice.startDate + " at " + dice.startTime;
-
-}
-function setMessage(counter)
-{
-
-  return "(It took you " + counter + " tries and 93 seconds ) ";
-}
-
-
 (function diceGame() {
 
-    // Use document.querySelector('...') to find HTML elements
-    // The argument into the function is a CSS selector!
-    // Any HTML element has an `innerText` property that you can
-    // access (or change) for the contents of that element.
+  var ettoreGame = {
+    ettoreStart: 0,
+    ettoreRound: []
 
-document.querySelector('button').addEventListener('click', function buttonClicked() {
-document.querySelector('.game-start').innerText = setDateTime();
+  }
+  var dice = {};
+  var diceArray = [];
 
-        var dieNumOne = document.querySelector('.dice aside:first-child').innerText;
-
-        var dieNumTwo = document.querySelector('.dice aside:last-child').innerText;
-
-        var dieOne = Math.floor(Math.random()* 6 + 1);
-
-        var dieTwo = Math.floor(Math.random()* 6 + 1);
-
-        var score = dieOne + dieTwo;
-        counter++;
+  function setDateTime(){
+    var now = new Date();
+    dice.startDate = ( '' + now.getFullYear() + "-" + (now.getMonth()+ 1) + "-" + now.getDate() + " " + now.getHours() +":"+ now.getMinutes());
+    return "Game Started " + dice.startDate;
+  }
+  document.querySelector('.game-start').innerText = setDateTime();
 
 
-        console.log(score);
-        document.querySelector('.dice aside:first-child').innerText = dieOne;
-        document.querySelector('.dice aside:last-child').innerText = dieTwo;
+  // Use document.querySelector('...') to find HTML elements
+  // The argument into the function is a CSS selector!
+  // Any HTML element has an `innerText` property that you can
+  // access (or change) for the contents of that element.
 
-        if (score === 11 || score === 7) {
-          document.querySelector('.result').innerText = "Winner!"
-          document.querySelector('.message').innerText = setMessage(counter);
-          var now = new Date();
-          var endGame =  ( now.getHours() +":"+ now.getMinutes() );
-          
-          counter = 0;
-        }
-        else{
-          document.querySelector('.result').innerText = "Try Again"
-          document.querySelector('.message').innerText = "";
-        }
-        ;
+  document.querySelector('button').addEventListener('click', function buttonClicked() {
 
-        // else if (score === 11) {
-        //   alert("You Win!!!")
-        // };
+    function setMessage()
+    {
 
+      return "(It took you " + ettoreGame.ettoreRound.length + " tries and 93 seconds ) ";
+    }
+
+    var dieOne = Math.floor(Math.random()* 6 + 1);
+
+    var dieTwo = Math.floor(Math.random()* 6 + 1);
+
+    var score = dieOne + dieTwo;
+
+    ettoreGame.ettoreRound.push(score);
 
 
+    console.log(score);
+    document.querySelector('.dice aside:first-child').innerText = dieOne;
+    document.querySelector('.dice aside:last-child').innerText = dieTwo;
 
+    if (score === 11 || score === 7) {
+      document.querySelector('.result').innerText = "Winner!"
+      document.querySelector('.message').innerText = setMessage();
+      ettoreGame.ettoreRound=[];
+      var now = new Date();
+      var endGame =  ( now.getHours() +":"+ now.getMinutes() );
 
-
-
+      counter = 0;
+    }
+    else{
+      document.querySelector('.result').innerText = "Try Again"
+      document.querySelector('.message').innerText = "";
+    }
 
 
     });
